740daf9c8506faffd6cdadd11b9dada5f0f8dfcf
Andrew <kroomz@mail.ru>
2025-05-03 15:16:42 +0300

    added ruff

--- main.py
+++ main.py
@@ -2,26 +2,19 @@ from fastapi import FastAPI, APIRouter
 
 router = APIRouter()
 
+
 @router.get("/")
 async def root():
-    return {
-        "data": "health_check"
-    }
+    return {"data": "health_check"}
+
 
 @router.get("/check_reload")
 async def reload_check():
-    return {
-        "reload": True
-    }
+    return {"reload": True}
 
 
 def create_app():
-    app = FastAPI(
-        debug=True,
-        description="Boiler_plate"
-    )
+    app = FastAPI(debug=True, description="Boiler_plate")
     app.include_router(router)
 
     return app
-
-
